8fb6f75c4e0e8c4652733407c760fca7211dcc3d
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-02-19 12:12:27 -0300

    add controller and services to DI tutorial project

--- src/backend/marcos.silva/a3.dependency-injection/src/main.ts
+++ src/backend/marcos.silva/a3.dependency-injection/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ComputerModule } from "./computer/computer.module";
+//////////////////////////////////////////////////////////////////////////////////////
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+/** bootstrap project */
+(async function () {
+  /** instantiate new project */
+  const app = await NestFactory.create(ComputerModule);
+
+  /** start server listener */
+  await app.listen(3000, () => console.log("Server running on port 3000."));
+})();
