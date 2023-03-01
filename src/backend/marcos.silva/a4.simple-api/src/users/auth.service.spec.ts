9d4bb8d31eed36e11f0ebb2285fbdffda266b3f8
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-03-01 12:05:37 -0300

    add tests to auth service spec @simple-api

--- src/backend/marcos.silva/a4.simple-api/src/users/auth.service.spec.ts
+++ src/backend/marcos.silva/a4.simple-api/src/users/auth.service.spec.ts
@@ -1,18 +1,26 @@
 import { Test } from "@nestjs/testing";
-import { AuthService } from "./auth.service";
+import { ConflictException } from "@nestjs/common";
+
 import { User } from "./users.entity";
+import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 //////////////////////////////////////////////////////////////////////////////////////
 
 describe("AuthService", () => {
   /** dependency instances */
   let service: AuthService;
+  let _usersService: Partial<UsersService>;
 
   /** dummy dependency instance and module */
   beforeEach(async () => {
-    const _usersService: Partial<UsersService> = {
+    _usersService = {
       find: () => Promise.resolve([]),
-      create: (body: Partial<User>) => Promise.resolve({} as User),
+      create: (body: Partial<User>) =>
+        Promise.resolve({
+          id: 1,
+          email: body.email,
+          password: body.password,
+        } as User),
     };
 
     const _module = await Test.createTestingModule({
@@ -28,5 +36,25 @@ describe("AuthService", () => {
   /** test suite */
   it("can create an instance of AuthService", async () => {
     expect(service).toBeDefined();
+  });
+
+  it("creates new user with salted and hashed password", async () => {
+    const email = "test@example.com";
+    const password = "test1234";
+
+    const _user = await service.signup({ email, password });
+    expect(_user.password).not.toEqual(password);
+
+    const [_salt, _hash] = _user.password.split(".");
+
+    expect([_salt, _hash]).toBeDefined();
+  });
+
+  it("throws an error if user signs up with email already in use", async () => {
+    _usersService.find = () => Promise.resolve([{} as User]);
+
+    await expect(
+      service.signup({ email: "test@example.com", password: "test1234" })
+    ).rejects.toThrow(ConflictException);
   });
 });
