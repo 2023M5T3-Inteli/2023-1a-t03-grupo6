803f51d1664f974007ae0f8e531de80c7d5f7095
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-03-08 11:38:16 -0300

    create api documentation

--- src/backend/ana.zaidan/dellmatch/src/main.ts
+++ src/backend/ana.zaidan/dellmatch/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Dell match - API Doc')
+    .setDescription('This is the Dell match API documentation, from group 6')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3306);
 }
 bootstrap();
