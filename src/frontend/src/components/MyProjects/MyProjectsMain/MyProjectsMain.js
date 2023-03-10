bc768766482120d86ce88b8c5d40bdb59478b328
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2023-03-10 10:01:03 -0300

    update style

---
+++ src/frontend/src/components/MyProjects/MyProjectsMain/MyProjectsMain.js
@@ -0,0 +1,33 @@
+import LikedProject from "../LikedProjects/LikedProject";
+import YourProjects from "../YourProjects/YourProjects";
+
+import styles from "./MyProjectsMain.module.scss";
+
+const MyProjectsMain = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.ProjectsHeader}>
+          <div className={styles.btnBx}>
+            <button
+              className={styles.likedProjectBtn}
+              onClick={props.showLikedProjectsHandler}
+            >
+              Your Liked Projects
+            </button>
+            <span> | </span>
+            <button
+              className={styles.yourProjectBtn}
+              onClick={props.showLikedProjectsHandler}
+            >
+              Your Projects
+            </button>
+          </div>
+        </div>
+        {props.showLikedProjects ? <LikedProject /> : <YourProjects />}
+      </div>
+    </div>
+  );
+};
+
+export default MyProjectsMain;
