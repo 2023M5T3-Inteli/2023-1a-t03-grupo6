d122f8f73d1c7590194e23444a9b7114f3a809e9
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2023-04-04 16:34:28 -0300

    style faq and my projects

--- src/frontend/src/components/MyProjects/YourProjects/YourProjects.js
+++ src/frontend/src/components/MyProjects/YourProjects/YourProjects.js
@@ -6,47 +6,12 @@ const LikedProject = () => {
   return (
     <div className={styles.ProjectsList}>
         <div>
-          <div style={{display: 'flex'}}>
-            <p style={{whiteSpace:'nowrap', paddingLeft: 0}}>Cloud computing service development</p>
-            <div style={{justifyContent: 'flex-end'}}>
-              <p style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)', paddingRight: 15}} className={styles.SugKeyWords}>In progress</p>
-            </div>
-          </div> 
+          <p>Cloud computing service development <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}}>In progress</div> </p>
           <div>
             <span>AWS</span>
             <span>Nest.js</span>
             <span>TypeScript</span>
-            <p style={{color: 'var(--blue)', paddingTop: '0px'}}>Approved</p>
-          </div>
-          <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
-        </div>
-        <div>
-          <p>Cloud computing service development <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}} className={styles.SugKeyWords}>In progress</div> </p>
-          <div>
-            <span>AWS</span>
-            <span>Nest.js</span>
-            <span>TypeScript</span>
-            <p style={{color: 'var(--blue)', paddingTop: '0px'}}>Approved</p>
-          </div>
-          <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
-        </div>
-        <div>
-          <p>Cloud computing service development <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}} className={styles.SugKeyWords}>In progress</div> </p>
-          <div>
-            <span>AWS</span>
-            <span>Nest.js</span>
-            <span>TypeScript</span>
-            <p style={{color: 'var(--blue)', paddingTop: '0px'}}>Approved</p>
-          </div>
-          <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
-        </div>
-        <div>
-          <p>Cloud computing service development <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}} className={styles.SugKeyWords}>In progress</div> </p>
-          <div>
-            <span>AWS</span>
-            <span>Nest.js</span>
-            <span>TypeScript</span>
-            <p style={{color: 'var(--blue)', paddingTop: '0px'}}>Approved</p>
+            <p style={{color: 'var(--blue)'}}>Approved</p>
           </div>
           <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
         </div>
@@ -55,35 +20,17 @@ const LikedProject = () => {
           <div>
             <span>Python</span>
             <span>Jupyter</span>
-            <p style={{color: 'var(--orange)', paddingTop: '0px'}}>Owner</p>
+            <p style={{color: 'var(--orange)'}}>Owner</p>
           </div>
           <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
         </div>
         <div>
-          <p>Logic for prediction with artificial intelligence <div style={{color: 'var(--dell-purple)', 'border-color': 'var(--dell-purple)'}}>Recruitment</div> </p>
-          <div>
-            <span>Python</span>
-            <span>Jupyter</span>
-            <p style={{color: 'var(--orange)', paddingTop: '0px'}}>Owner</p>
-          </div>
-          <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
-        </div>
-        <div>
-          <p>Logic for prediction with artificial intelligence <div style={{color: 'var(--dell-purple)', 'border-color': 'var(--dell-purple)'}}>Recruitment</div> </p>
-          <div>
-            <span>Python</span>
-            <span>Jupyter</span>
-            <p style={{color: 'var(--orange)', paddingTop: '0px'}}>Owner</p>
-          </div>
-          <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
-        </div>
-        <div>
-          <p>IoT solution prototyping <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}} className={styles.SugKeyWords}>In progress</div> </p>
+          <p>IoT solution prototyping <div style={{color: 'var(--dell-berry)', 'border-color': 'var(--dell-berry)'}}>In progress</div> </p>
           <div>
             <span>C++</span>
             <span>Arduino IDE</span>
             <span>MongoDB</span>
-            <p style={{color: 'var(--red)', paddingTop: '0px'}}>Rejected</p>
+            <p style={{color: 'var(--red)'}}>Rejected</p>
           </div>
           <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet a lacus eget hendrerit. Cras non convallis quam, sit amet congue risus. Pellentesque quis accumsan dolor. Donec volutpat libero dui, vel accumsan nunc volutpat ut. Morbi id feugiat est, sed aliquet ante.</p></span>
         </div>
