6c14972f844e6dacf867a03d5b1eea7789927f07
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2023-04-05 15:20:13 -0300

    test

--- src/backend/dell-match/src/main.ts
+++ src/backend/dell-match/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from "./app.module";
 
   app.enableCors({
     origin: true,
-    methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
+    methods: 'GET,POST,DELETE,PATCH,OPTIONS',
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
